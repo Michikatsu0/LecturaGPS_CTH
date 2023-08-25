df0bfc6eadeeab8165c0a786c2eb0c7cd45faee5
Gogo <68073260+Michikatsu0@users.noreply.github.com>
2023-08-25 15:16:02 -0500

    Update main.cpp

--- GPS2/src/main.cpp
+++ GPS2/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
 
 enum class StatesMachine { SMART_DELAY, CAL_TEMP, CAL_HUM, PRUN_TEMP, PRUN_HUM };
 StatesMachine StateMachine = StatesMachine::SMART_DELAY;
@@ -10,55 +11,55 @@ TinyGPS gps;
 
 bool newData = false;
 int iterations = 5;
-
+double dataTemp;
+double dataHume;
 static void smartdelay(unsigned long ms);
 
-void setup() {
-   // put your setup code here, to run once:
+void setup() 
+{
+  // put your setup code here, to run once:
   gpsserial.begin(9600);
   Serial.begin(115200);
 }
 
 void loop()
 {
-  newData = false;
-  
-  switch (StateMachine)
+  if (!newData)
   {
-  case StatesMachine::SMART_DELAY:
-  {
-    smartdelay(100);
-    break;
-  }
-  case StatesMachine::CAL_TEMP:
-  {
-    
-    break;
-  }
-  
-  default:
-    break;
-  }
+    switch (StateMachine)
+    {
+      case StatesMachine::SMART_DELAY:
+      {
+        smartdelay(100);
+        StateMachine = StatesMachine::CAL_TEMP;          break;
+      }
+      case StatesMachine::CAL_TEMP:
+      {
+        for (int i = 0; i < iterations; i++)
+        {
+          //dataTemp += Sens
+        }
+        break;
+      }
 
-
-  if (newData)
+        default:
+          break;
+    }
+  }
+  else
   {
     float flat, flon;
     unsigned long age;
     gps.f_get_position(&flat, &flon, &age);
     Serial.print("LAT=");
     Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
+    Serial.print("LON=");
     Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" ALT=");
-    Serial.print(age == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : age, 6);
-    print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-    print_date(gps);
-    print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-    Serial.print(" HORA=");
-    
+    Serial.print("ALT=");
+    Serial.print(gps.f_altitude());
+    Serial.print("AGE");
+    Serial.print(age == TinyGPS::GPS_INVALID_AGE ? 0.0 : age, 5);
   }
-  
 }
 
 
@@ -73,6 +74,8 @@ static void smartdelay(unsigned long ms)
       // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
       if (gps.encode(c)) // Did a new valid sentence come in?
         newData = true;
+      else
+        newData = false;
     }
   }
 }
